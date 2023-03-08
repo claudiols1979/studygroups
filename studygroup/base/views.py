3e70684fabfb22d1e1f2c500e5b3662f28c42196
claudiols1979 <claudiols1979@hotmail.com>
2023-03-08 12:26:16 -0600

    work activity component

--- studygroup/base/views.py
+++ studygroup/base/views.py
@@ -177,4 +177,4 @@ def topicsPage(request):
 
 def activityPage(request):
     room_messages = Message.objects.all()
-    return render(request, 'base/activity.html', {})
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
